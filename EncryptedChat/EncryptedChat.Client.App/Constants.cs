073322d2a5d47c34bb3ca5a55f25595c6d099a85
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-07-18 21:02:41 +0300

    Add constants

---
+++ EncryptedChat/EncryptedChat.Client.App/Constants.cs
@@ -0,0 +1,8 @@
+namespace EncryptedChat.Client.App
+{
+    public static class Constants
+    {
+        public const string ServerUrl = "https://ench.azurewebsites.net/chat";
+        public const string ExitCommand = "/e";
+    }
+}
