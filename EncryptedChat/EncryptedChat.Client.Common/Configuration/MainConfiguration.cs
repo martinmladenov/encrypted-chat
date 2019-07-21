9af6b4a5822813a230bea5d06982af36b744f8e3
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-07-21 16:44:17 +0300

    Implement trusted users & user identification

--- EncryptedChat/EncryptedChat.Client.Common/Configuration/MainConfiguration.cs
+++ EncryptedChat/EncryptedChat.Client.Common/Configuration/MainConfiguration.cs
@@ -1,9 +1,14 @@
 namespace EncryptedChat.Client.Common.Configuration
 {
+    using System.Collections.Generic;
+
     public class MainConfiguration
     {
         public string Username { get; set; }
 
         public string PrivateKey { get; set; }
+
+        public IDictionary<string, string> TrustedUsers { get; set; }
+            = new Dictionary<string, string>();
     }
 }
