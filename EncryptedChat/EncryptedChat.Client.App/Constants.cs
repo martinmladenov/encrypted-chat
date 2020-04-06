4e9b89638455e8b14f7c2108911a993d339465a1
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2020-04-06 15:51:55 +0300

    Change config file name

--- EncryptedChat/EncryptedChat.Client.App/Constants.cs
+++ EncryptedChat/EncryptedChat.Client.App/Constants.cs
@@ -2,7 +2,7 @@ namespace EncryptedChat.Client.App
 {
     public static class Constants
     {
-        public const string ConfigurationFilePath = "EncryptedChat.config";
+        public const string ConfigurationFilePath = "encrypted-chat-config.json";
         public const string ServerUrl = "https://ench.azurewebsites.net/chat";
         public const string ExitCommand = "/e";
         public const string TrustCommand = "/trust";
