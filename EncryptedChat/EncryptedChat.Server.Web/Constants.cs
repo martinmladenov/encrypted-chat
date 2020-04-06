e58f1873bea260c7b1b6087286868cdf0072ed2e
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2020-04-06 14:43:49 +0300

    Redirect all direct requests to server to external URL

---
+++ EncryptedChat/EncryptedChat.Server.Web/Constants.cs
@@ -0,0 +1,7 @@
+namespace EncryptedChat.Server.Web
+{
+    public static class Constants
+    {
+        public const string RedirectUrl = "https://github.com/martinmladenov/encrypted-chat";
+    }
+}
