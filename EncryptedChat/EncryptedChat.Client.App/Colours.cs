0cf6b312ba603de2440a5883975646dbadfdb429
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2020-04-07 15:27:43 +0300

    Add Colours.cs

---
+++ EncryptedChat/EncryptedChat.Client.App/Colours.cs
@@ -0,0 +1,12 @@
+namespace EncryptedChat.Client.App
+{
+    using System;
+
+    public static class Colours
+    {
+        public const ConsoleColor Info = ConsoleColor.Gray;
+        public const ConsoleColor Warning = ConsoleColor.Red;
+        public const ConsoleColor Success = ConsoleColor.Green;
+        public const ConsoleColor Message = ConsoleColor.White;
+    }
+}
